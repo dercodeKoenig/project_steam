ab9c0fd799b764603215efd320ab271a49d53c68
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2024-12-16 21:46:40 +0100

    .

--- src/main/java/ProjectSteam/Static.java
+++ src/main/java/ProjectSteam/Static.java
@@ -5,6 +5,8 @@ import com.mojang.blaze3d.vertex.VertexFormatElement;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.loading.FMLEnvironment;
 
 public class Static {
 
@@ -17,16 +19,20 @@ public class Static {
     // the order in that you define it is very important!
     // unlike older versions, the order is not linked to .addvertex(...) but has to be like this
     // I dont know why but this is the only way it works
-    public static final VertexFormat POSITION_COLOR_TEXTURE_NORMAL_LIGHT =
-            VertexFormat.builder()
-                    .add("Position", VertexFormatElement.POSITION)
-                    .add("Color", VertexFormatElement.COLOR)
-                    .add("UV0", VertexFormatElement.UV0)
-                    .add("UV1", VertexFormatElement.UV1)
-                    .add("UV2", VertexFormatElement.UV2)
-                    .add("Normal", VertexFormatElement.NORMAL)
-                    .build();
-
+    public static VertexFormat POSITION_COLOR_TEXTURE_NORMAL_LIGHT = null;
+    static {
+        if(FMLEnvironment.dist == Dist.CLIENT) {
+            POSITION_COLOR_TEXTURE_NORMAL_LIGHT =
+                    VertexFormat.builder()
+                            .add("Position", VertexFormatElement.POSITION)
+                            .add("Color", VertexFormatElement.COLOR)
+                            .add("UV0", VertexFormatElement.UV0)
+                            .add("UV1", VertexFormatElement.UV1)
+                            .add("UV2", VertexFormatElement.UV2)
+                            .add("Normal", VertexFormatElement.NORMAL)
+                            .build();
+        }
+    }
     public static ShaderInstance ENTITY_SOLID_SHADER_CLONE_WITH_DYNAMIC_NORMAL;
     public static ShaderInstance getEntitySolidDynamicNormalShader(){return ENTITY_SOLID_SHADER_CLONE_WITH_DYNAMIC_NORMAL;}
 

abcc8c6f8faa7a7332ccccff8e2163ea89d89f29
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2024-12-22 20:34:57 +0100

    Update EntityWoodMill.java

--- crafting/src/main/java/ProjectSteamCrafting/WoodMill/EntityWoodMill.java
+++ crafting/src/main/java/ProjectSteamCrafting/WoodMill/EntityWoodMill.java
@@ -387,7 +387,7 @@ public class EntityWoodMill extends EntityMultiblockMaster implements ProjectSte
 
                 for (workingRecipe i : currentWorkingRecipes) {
                     if ((i.progress - (int) i.progress > offset && i.progress - progressMade - (int) i.progress < offset)||(i.progress - (int) i.progress > offset+0.5 && i.progress - progressMade - (int) i.progress < offset+0.5)) {
-                        level.playSound(null, getBlockPos(), SoundEvents.FENCE_GATE_OPEN, SoundSource.BLOCKS, 0.1f, 0.5f);
+                        level.playSound(null, getBlockPos(), SoundEvents.FENCE_GATE_OPEN, SoundSource.BLOCKS, 0.2f, 0.5f);
                     }
                 }
             }

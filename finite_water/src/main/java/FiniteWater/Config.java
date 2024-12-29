6d57b987b12338f60abc80ffb1863c424396fc10
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2024-12-29 18:27:29 +0100

    ported finite water to 1.21

---
+++ finite_water/src/main/java/FiniteWater/Config.java
@@ -0,0 +1,73 @@
+package FiniteWater;
+
+import ResearchSystem.Config.RecipeConfig;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.neoforged.fml.loading.FMLPaths;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Config {
+
+    public static Config INSTANCE = loadConfig();
+
+    public String _comment = "when isBlackList is set to false, only biomes in the list are allowed to from water sources. If isBlackList is set to true, only biomes in the list are prevented from forming water sources.";
+    public boolean isBlackList = false;
+    public Set<String> biomes = new HashSet<>();
+
+    public Config() {
+        biomes.add("minecraft:river");
+        biomes.add("minecraft:frozen_river");
+
+        biomes.add("minecraft:ocean");
+        biomes.add("minecraft:frozen_ocean");
+        biomes.add("minecraft:cold_ocean");
+        biomes.add("minecraft:lukewarm_ocean");
+        biomes.add("minecraft:warm_ocean");
+
+        biomes.add("minecraft:deep_ocean");
+        biomes.add("minecraft:deep_frozen_ocean");
+        biomes.add("minecraft:deep_cold_ocean");
+        biomes.add("minecraft:deep_lukewarm_ocean");
+
+        biomes.add("minecraft:swamp");
+
+        biomes.add("minecraft:snowy_beach");
+        biomes.add("minecraft:stony_shore");
+        biomes.add("minecraft:beach");
+
+    }
+
+    public static Config loadConfig() {
+        String filename = "finite_water.json";
+        Path configDir = Paths.get(FMLPaths.CONFIGDIR.get().toString());
+        Path filePath = configDir.resolve(filename);
+
+        try {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            if (!Files.exists(filePath)) {
+                Files.createFile(filePath);
+                String json = gson.toJson(new Config());
+                Files.writeString(filePath, json);
+                System.out.println("Created finite_water config file: " + filePath);
+            }
+            String json = Files.readString(filePath);
+            Config c = gson.fromJson(json, Config.class);
+            System.out.println("Loaded finite_water config file: " + filePath);
+            return c;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ finite_water/src/main/java/FiniteWater/Main.java
@@ -0,0 +1,46 @@
+package FiniteWater;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.biome.Biome;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.block.CreateFluidSourceEvent;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Mod("finite_water")
+public class Main {
+
+    public Main(IEventBus modEventBus, ModContainer modContaine) throws IOException {
+        NeoForge.EVENT_BUS.addListener(this::onSourceCreate);
+    }
+
+    private void onSourceCreate(CreateFluidSourceEvent e) {
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        if(server != null) {
+            Holder<Biome> h = e.getLevel().getBiome(e.getPos());
+            ResourceLocation id =  server.registryAccess().registryOrThrow(Registries.BIOME).getKey(h.value());
+            String idString = id.toString();
+
+            if(Config.INSTANCE.biomes.contains(idString)){
+                if(Config.INSTANCE.isBlackList){
+                    e.setCanConvert(false);
+                }
+            }else{
+                if(!Config.INSTANCE.isBlackList){
+                    e.setCanConvert(false);
+                }
+            }
+        }
+    }
+}

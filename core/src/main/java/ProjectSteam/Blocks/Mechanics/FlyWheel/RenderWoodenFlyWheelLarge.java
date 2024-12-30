44ca88334e06eefc65241de9958cf2e672694cfe
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2024-12-30 13:00:27 +0100

    .

--- core/src/main/java/ProjectSteam/Blocks/Mechanics/FlyWheel/RenderWoodenFlyWheelLarge.java
+++ core/src/main/java/ProjectSteam/Blocks/Mechanics/FlyWheel/RenderWoodenFlyWheelLarge.java
@@ -1,10 +1,17 @@
 package ProjectSteam.Blocks.Mechanics.FlyWheel;
 
+import ProjectSteamAW2Generators.WindMill.EntityWindMillGenerator;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.phys.AABB;
 
 public class RenderWoodenFlyWheelLarge extends RenderLargeFlyWheelBase {
     public RenderWoodenFlyWheelLarge(BlockEntityRendererProvider.Context c) {
         super(c, ResourceLocation.fromNamespaceAndPath("projectsteam", "textures/block/planks.png"));
     }
+    @Override
+    public AABB getRenderBoundingBox(EntityFlyWheelBase tile) {
+        return new AABB(tile.getBlockPos()).inflate(1);
+    }
+
 }
